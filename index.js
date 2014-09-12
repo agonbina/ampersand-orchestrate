4125af6602a9285a77e6ef136d708e4e33a7a627
Agon Bina <agon_bina@hotmail.com>
2014-09-12 00:02:27 -0400

    Refactor index.js; attempt to grab the orchestrate token as a environment variable

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 
 
 module.exports = function (token) {
+	token = token || process.env.ORCHESTRATE_TOKEN;
     if(!token) return console.error('You must specify a token to connect to your Orchestrate Application');
 
     var db = require('orchestrate')(token);
 
-    var BaseModel = require('./lib/model')(db);
-    var BaseCollection = require('./lib/collection')(db);
-
-    db.Model = BaseModel;
-    db.Collection = BaseCollection;
+    db.Model = require('./lib/model')(db);
+    db.Collection = require('./lib/collection')(db);
 
     return db;
 };
