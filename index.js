5240349eb440e25647494fd9eb512c41eedfc87e
Agon Bina <agon_bina@hotmail.com>
2014-09-12 05:30:06 +0200

    Implemented fetch and fetchByKey for Collection

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 
-/**
- * Module dependencies
- */
-
-var Model = require('ampersand-model');
-var Collection = require('ampersand-collection');
 
 module.exports = function (token) {
     if(!token) return console.error('You must specify a token to connect to your Orchestrate Application');
 
     var db = require('orchestrate')(token);
 
-    // Extend Model to have a property 'key' as the idAttribute
-    var BaseModel = require('./lib/model/base-model')(Model, db);
-
-    BaseModel.Event = require('./lib/model/event')(BaseModel);
+    var BaseModel = require('./lib/model')(db);
+    var BaseCollection = require('./lib/collection')(db);
 
     db.Model = BaseModel;
+    db.Collection = BaseCollection;
 
     return db;
 };
